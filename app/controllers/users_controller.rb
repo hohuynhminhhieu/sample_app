0b632f75f53e75bc774012a4f5b22cd49473e469
HO HUYNH MINH HIEU <hohuynhminhhieu@gmail.com>
2017-10-09 12:02:56 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -23,5 +24,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end 
+    end
 end
