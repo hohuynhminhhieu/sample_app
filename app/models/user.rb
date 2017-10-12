bdcdb582d3740b23a6093798e4815d3b02700baf
HO HUYNH MINH HIEU <hohuynhminhhieu@gmail.com>
2017-10-12 12:31:06 +0700

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,9 +52,9 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(reset_digest:  FILL_IN, reset_sent_at: FILL_IN)
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
-
 
   # Sends password reset email.
   def send_password_reset_email
